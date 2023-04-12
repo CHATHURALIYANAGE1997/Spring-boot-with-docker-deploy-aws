8da1153f95b4a0c09bd8308aa658576da0bd919a
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2023-04-12 19:05:42 +0530

    Update MainController.java

--- src/main/java/com/example/springbootdockeraws/controller/MainController.java
+++ src/main/java/com/example/springbootdockeraws/controller/MainController.java
@@ -1,9 +1,11 @@
 package com.example.springbootdockeraws.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://44.210.141.60:3000")
 public class MainController {
     @RequestMapping(path = "/")
     public String index(){
